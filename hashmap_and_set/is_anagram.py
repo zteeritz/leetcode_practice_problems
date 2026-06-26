bff71dd9ffc286eadb8af5c44ada22da844fa4c4
teerit <teerit.p@deverhood.com>
2026-06-27 00:30:43 +0700

    refactor is_anagram implementation to use character frequency mapping

--- hashmap_and_set/is_anagram.py
+++ hashmap_and_set/is_anagram.py
@@ -1,9 +1,19 @@
-
 class Solution:
     def is_anagram(self, s: str, t: str) -> bool:
-        # TODO: "a" and "ab"
-        s_set = set(s)
-        t_set = set(t)
-        diff = s_set.difference(t_set)
-        return len(diff) == 0
+        s_map = self.create_map(s)
+        t_map = self.create_map(t)
+        return s_map == t_map
 
+    def create_map(self, s: str) -> dict[str, int]:
+        map = {}
+        for char in s:
+            if char not in map:
+                map[char] = 1
+            else:
+                map[char] += 1
+
+        return map
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.is_anagram("leetcode", "leetcode"))
