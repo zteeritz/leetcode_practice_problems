d02b83c3f3c77def2d8d7cb080e4375071814deb
zteeritz <teeritphys@gmail.com>
2026-07-09 00:54:30 +0700

    wip: add valid palindrome implementation with initial mismatch handling

---
+++ two_pointers/valid_palindrome_ll.py
@@ -0,0 +1,53 @@
+from operator import is_
+
+
+class Solution:
+    # should decide immediately at first mismatch and test both branches
+    def valid_palindrome_wrong_answer(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        count = 0
+        idx = 0
+        while left < right:
+            if s[left] != s[right]:
+                count += 1
+                idx = right
+
+            if count > 1:
+                return False
+            left += 1
+            right -= 1
+
+        pop_char = ''.join(char for i, char in enumerate(s) if i != idx)
+        left = 0
+        right = len(pop_char) - 1
+        while left < right:
+            if pop_char[left] != pop_char[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+    def is_palindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+    def valid_palindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            return self.is_palindrome()
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.valid_palindrome("abca"))
